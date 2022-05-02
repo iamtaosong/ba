db02942305538afbba5324fcefb3cc4fd5546122
iamtaosong <iamtaosong@googlemail.com>
2022-05-02 18:07:59 +0100

    Update test.tf

--- test.tf
+++ test.tf
@@ -13,6 +13,24 @@ resource "google_binary_authorization_attestor" "attestor" {
   }
 }
 
+ resource "null_resource" "this" {
+  count = var.ns_ba_policy == true ? 1 : 0
+  triggers = {
+    template = "${data.template_file.ba-policy[0].rendered}"
+  }
+  provisioner "local-exec" {
+    command = <<-EOT
+      tmp_d=$(mktemp -d)
+      tmp_f="$tmp_d/binary-authz-policy.yaml"
+       gcloud config set project ${var.project_id}
+      echo "${data.template_file.ba-policy[0].rendered}" > $tmp_f
+      gcloud container binauthz policy import $tmp_f
+      echo  "$tmp_d"
+    EOT
+  }
+}
+
+
 resource "google_binary_authorization_attestor" "kubeflow-namespace-attestor" {
   name = "gke-ns-kubflow-attestor"
   attestation_authority_note {
@@ -27,33 +45,18 @@ resource "google_binary_authorization_attestor" "kubeflow-namespace-attestor" {
   }
 }
 
-
-resource "null_resource" "this" {
-  triggers = {
-    template = "${data.template_file.ba-policy.rendered}"
-  }
-  provisioner "local-exec" {
-    command = <<-EOT
-      tmp_d=$(mktemp -d)
-      tmp_f="$tmp_d/binary-authz-policy.yaml"
-       gcloud config set project ${var.project_id}
-      echo "${data.template_file.ba-policy.rendered}" > $tmp_f
-      gcloud container binauthz policy import $tmp_f
-      echo  "$tmp_d"
-    EOT
-  }
-}
-
 data "template_file" "ba-policy" {
+  count    = var.ns_ba_policy == true ? 1 : 0
   template = "${file("./binary-authz-policy.tpl")}"
   vars = {
-    default_enforcementMode = "ENFORCED_BLOCK_AND_AUDIT_LOG"
-    default_evaluationMode  = "REQUIRE_ATTESTATION"
-    globalPolicyEvaluationMode  = "DISABLE"   
-    ns_kf_enforcementMode = "ENFORCED_BLOCK_AND_AUDIT_LOG"
-    ns_kf_evaluationMode = "REQUIRE_ATTESTATION"
-    default_attestor  = google_binary_authorization_attestor.attestor.name
-    kubeflow_attestor = google_binary_authorization_attestor.kubeflow-namespace-attestor.name
-    project_id        = var.project_id
+    default_enforcementMode    = "ENFORCED_BLOCK_AND_AUDIT_LOG"
+    default_evaluationMode     = "REQUIRE_ATTESTATION"
+    globalPolicyEvaluationMode = "DISABLE"
+    ns_kf_enforcementMode      = "ENFORCED_BLOCK_AND_AUDIT_LOG"
+    ns_kf_evaluationMode       = "REQUIRE_ATTESTATION"
+    default_attestor           = google_binary_authorization_attestor.attestor.name
+    kubeflow_attestor          = google_binary_authorization_attestor.kubeflow-namespace-attestor.name
+    project_id                 = var.project_id
   }
 }
+ 
